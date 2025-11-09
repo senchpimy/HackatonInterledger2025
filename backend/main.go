d0f2ae633b9a5d5044717aa4b1223427108d3c4b
senchpimy <senchpimy@gmail.com>
2025-11-08 18:54:24 -0600

    main go

---
+++ backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"gofundme-backend/handler" // Importar el paquete de manejadores
+	"gofundme-backend/store"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Inicializar la base de datos
+	store.InitDB("gofundme.db")
+	log.Println("Base de datos inicializada correctamente.")
+
+	r := mux.NewRouter()
+
+	// Rutas de la API
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/campaigns", handler.CreateCampaignHandler).Methods("POST")
+	api.HandleFunc("/campaigns", handler.GetCampaignsHandler).Methods("GET")
+	api.HandleFunc("/campaigns/{id:[0-9]+}", handler.GetCampaignHandler).Methods("GET")
+	api.HandleFunc("/campaigns/{id:[0-9]+}/donations", handler.CreateDonationHandler).Methods("POST")
+
+
+	// Ruta de verificación de estado
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "API de GoFundMe está en funcionamiento")
+	}).Methods("GET")
+
+	log.Println("Servidor escuchando en http://localhost:8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
+}
